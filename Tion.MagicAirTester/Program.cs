882d8e7adae71ebe618934c6a76d56f99861911a
Alex Kutkin <excussor@gmail.com>
2017-05-15 12:52:14 +0700

    log4net included

--- Tion.MagicAirTester/Program.cs
+++ Tion.MagicAirTester/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Autofac;
+using log4net;
+using log4net.Repository.Hierarchy;
 using Tion.DeviceTester;
 using Tion.DeviceTester.Infrastructure.Factories;
 
@@ -12,6 +15,7 @@ namespace Tion.MagicAirTester
     static class Program
     {
         private static IContainer _container;
+        private static ILog Logger { get; set; }
 
         /// <summary>
         /// The main entry point for the application.
@@ -19,6 +23,7 @@ namespace Tion.MagicAirTester
         [STAThread]
         static void Main()
         {
+            Logger = LogManager.GetLogger(Assembly.GetExecutingAssembly(), "Application");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             _container = Bootstrapper.Initialize();
